e5d8cbd14fa58462218d5ce80a1383da6e6e1bd4
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-07-26 19:00:41 +0200

    add linting and code styles

--- README.md
+++ README.md
@@ -1,13 +1,32 @@
 # symportal_kitchen
 Python package as a collection of functions to maintain SymPortal Web Application and Computation Framework
 
-# Get Started
+# Developer Guide
+
+## Get Started
 
 Prepare `.env` file
 
 ```commandline
 SENDGRID_API_KEY=''
 SENDGRID_EMAIL_SENDER=''
+```
+
+# Code Reformatting and Style Checks
+
+## Run Formatter
+
+```commandline
+poetry run black symportal_kitchen
+poetry run autopep8 --recursive --in-place symportal_kitchen
+```
+
+## Run Linter
+
+```commandline
+poetry run flake8 symportal_kitchen
+poetry run pylint symportal_kitchen
+poetry run mypy symportal_kitchen
 ```
 
 # Usage
--- symportal_kitchen/email_notifications/submission_status_update.py
+++ symportal_kitchen/email_notifications/submission_status_update.py
@@ -8,11 +8,11 @@ from jinja2 import Environment, FileSystemLoader
 # Load environment variables from .env file
 load_dotenv()
 
-SENDGRID_EMAIL_SENDER = os.environ.get('SENDGRID_EMAIL_SENDER')
+SENDGRID_EMAIL_SENDER = os.environ.get("SENDGRID_EMAIL_SENDER")
 
 
 def read_email_template(template_name):
-    template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+    template_dir = os.path.join(os.path.dirname(__file__), "templates")
     env = Environment(loader=FileSystemLoader(template_dir))
     template = env.get_template(template_name)
     return template
@@ -23,19 +23,21 @@ def send_email(to_email, submission_status, from_email=SENDGRID_EMAIL_SENDER):
     subject = "Status Update for Your Submission"
     substitutions = {
         "subject": subject,
-        "recipient_name": to_email.split('@')[0],  # Just using the part before '@' as the recipient name
-        "submission_status": submission_status
+        "recipient_name": to_email.split("@")[
+            0
+        ],  # Just using the part before '@' as the recipient name
+        "submission_status": submission_status,
     }
 
     # Read and render the email template
-    template = read_email_template('status_update_email_template.html')
+    template = read_email_template("status_update_email_template.html")
     template_content = template.render(substitutions)
 
     message = Mail(
         from_email=from_email,
         to_emails=to_email,
         subject=subject,
-        html_content=template_content
+        html_content=template_content,
     )
 
     # Before sending the email
@@ -43,14 +45,15 @@ def send_email(to_email, submission_status, from_email=SENDGRID_EMAIL_SENDER):
 
     # Send the email
     try:
-        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        sg = SendGridAPIClient(os.environ.get("SENDGRID_API_KEY"))
         response = sg.send(message)
         if response.status_code == 202:
-            logging.debug('Email sent successfully!')
+            logging.debug("Email sent successfully!")
             return True
         else:
-            logging.error(f'Failed to send email. Status code: {response.status_code}')
+            logging.error(
+                f"Failed to send email. Status code: {response.status_code}")
             return False
     except Exception as e:
-        logging.error(f'Error: {str(e)}')
+        logging.error(f"Error: {str(e)}")
         return False
