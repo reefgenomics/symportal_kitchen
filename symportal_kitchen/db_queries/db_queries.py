bb62715339c87ca50d785006b82396a814045ed0
greenjune-ship-it <yulia_iakovleva@proton.me>
2023-08-15 18:25:09 +0200

    create the part for db queries

--- symportal_kitchen/db_queries/db_queries.py
+++ symportal_kitchen/db_queries/db_queries.py
@@ -1,2 +1,20 @@
 def get_user_by_id(user_model, id):
-    return user_model.objects.get(id=id)
+    user_object = None
+
+    try:
+        # Using the get method to retrieve the user by id
+        user_object = user_model.objects.get(id=id)
+    except Exception:
+        pass
+
+    if not user_object:
+        try:
+            # Using the query filter to retrieve the user by id
+            user_object = user_model.query.filter_by(id=id).one()
+        except Exception:
+            pass
+
+    if user_object is None:
+        raise ValueError(f'User not found for the given ID: {id}')
+
+    return user_object
